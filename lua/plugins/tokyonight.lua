4761b8176319acdea24409e826207307873411d0
Owes <mdowes@outlook.com>
2025-11-13 19:55:25 +0530

    refactor: configure tokyonight with explicit setup options
    - Configure tokyonight with explicit setup options and remove the lazy-loading flag.

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -1,17 +1,27 @@
-return { -- You can easily change to a different colorscheme.
-		-- Change the name of the colorscheme plugin below, and then
-		-- change the command in the config to whatever the name of that colorscheme is.
-		--
-		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-		"folke/tokyonight.nvim",
-		priority = 1000, -- Make sure to load this before all the other start plugins.
-		init = function()
-			-- Load the colorscheme here.
-			-- Like many other themes, this one has different styles, and you could load
-			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-			vim.cmd.colorscheme("tokyonight-night")
+return {
+	"folke/tokyonight.nvim",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		local tokyonight = require("tokyonight")
 
-			-- You can configure highlights by doing something like:
-			vim.cmd.hi("Comment gui=none")
-		end,
-	}
+		tokyonight.setup({
+			style = "night", -- "storm", "moon", "night", or "day"
+			transparent = false, -- Set to true if you want transparency
+			terminal_colors = true,
+			styles = {
+				functions = {}, -- Disable italic for functions
+			},
+			on_colors = function(colors)
+				colors.hint = colors.orange
+				colors.error = "#ff0000"
+			end,
+			on_highlights = function(highlights, colors)
+				highlights.TSFunction = { italic = true, fg = colors.blue }
+			end,
+		})
+
+		vim.cmd.colorscheme("tokyonight-night")
+		vim.opt.background = "dark"
+	end,
+}
