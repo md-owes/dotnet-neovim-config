b34feb1546bd21fe5a06a29c8b36532f9bfc13db
Owes <mdowes@outlook.com>
2025-12-17 13:25:31 +0530

    feat: adjust keybindings for inlay hints and Supermaven, and add a new which-key group for UI toggles.

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -151,11 +151,11 @@ return {
 				--
 				-- This may be unwanted, since they displace some of your code
 				if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
-					map("<leader>th", function()
+					map("<leader>uh", function()
 						vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({
 							bufnr = event.buf,
 						}))
-					end, "[T]oggle Inlay [H]ints")
+					end, "[U]I: Toggle [H]ints")
 				end
 			end,
 		})
